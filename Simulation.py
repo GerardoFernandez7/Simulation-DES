264b8491f3628bac5992725c73367e9cbfdbb88c
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2024-02-27 15:59:55 -0600

    Se agrego la funcion que crea los procesos

--- Simulation.py
+++ Simulation.py
@@ -2,16 +2,23 @@ import simpy
 import random
 
 env = simpy.Environment()
-ram = simpy.Container(env, init=100, capacity=100)
-cpu = simpy.Resource(env, capacity=1)
+ram = simpy.Container(env, init = 100, capacity = 100)
+cpu = simpy.Resource(env, capacity = 1)
 random.seed(40)
-cantProcesos = 25
+cant_Procesos = 25
 
 class Simulation:
-    
-    # Constructor de un proceso
-    def __init__(self, id, cantRam, cantInstrucciones):
-        self.id =id
-        self.cantRam = cantRam
-        self.cantInstrucciones = cantInstrucciones
 
+    # Constructor de un proceso
+    def initialize(self, id, cant_Ram, cant_Instrucciones):
+        self.id = id
+        self.cant_Ram = cant_Ram
+        self.cant_Instrucciones = cant_Instrucciones
+
+# Funcion que crea los procesos 
+def crear_Procesos():
+    for i in range(cant_Procesos):
+        intervalo = 10
+        yield env.timeout(random.expovariate(1.0 / intervalo))
+        proceso = Simulation(i, random.randint(1, 10), random.randint(1, 10))
+        env.process(new(proceso))
